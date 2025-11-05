c799c78405a4ff610cc2536946c64e26b3fe4c3f
Diego Koivukangas <diegokvka@gmail.com>
2025-11-05 10:49:51 +0200

    refactored config parsing, implemented gateway startup

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,13 +2,21 @@ package server
 
 import (
 	"dgateway/internal/config"
+	"dgateway/internal/handler"
+	"net/http"
 )
 
 // Entry point
-func Run(configPath string) error {
-	cfg, err := config.Get(configPath)
-	if err != nil {
-		return err
+func Run(cfg *config.Config) error {
+	mux := http.NewServeMux()
+
+	for _, servercfg := range cfg.ServerPool {
+		if servercfg.AllowWebsocket {
+			mux.Handle(servercfg.GatewayEndpoint, handler.CreateWithWebsocket(servercfg.Destination))
+		} else {
+			mux.Handle(servercfg.GatewayEndpoint, handler.Create(servercfg.Destination))
+		}
 	}
 
+	return http.ListenAndServe(cfg.Gateway.Port, mux)
 }
